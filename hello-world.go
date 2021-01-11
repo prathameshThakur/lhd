a55f60a4618dfa834ed71da7113d7807742316c4
Prathamesh Thakur <prathamesh272000@gmail.com>
2021-01-11 21:12:51 +0530

    Create hello-world.go

---
+++ hello-world.go
@@ -0,0 +1,16 @@
+// Here we print the classic "hello world" message. 
+
+// To run the program, put the code in hello-world.go and use go run.
+// $ go run hello-world.go
+// To build our programs into binaries. We can do this using go build.
+// $ go build hello-world.go
+
+// Here's the full source code.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("hello world!!!")
+}
