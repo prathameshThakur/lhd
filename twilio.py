ce6d834a06c78e9377e6103132698c0695199690
Prathamesh Thakur <prathamesh272000@gmail.com>
2021-01-13 20:39:20 +0530

    add code for using twilio api in python

---
+++ twilio.py
@@ -0,0 +1,18 @@
+# You will find your ACCOUNT SID, AUTH TOKEN, twilio PHONE NUMBER on the twilio dashboard..
+
+from twilio.rest import Client
+
+# Your Account SID from twilio.com/console
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  # ACCOUNT SID
+# Your Auth Token from twilio.com/console
+auth_token  = "8XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  # AUTH TOKEN
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+913874020472", 
+    from_="+6547385345", # PHONE NUMBER
+    # Enter your message in body
+    body="Hello from Python!\nTwilio api is working!!!! :)")
+
+print(message.sid)
