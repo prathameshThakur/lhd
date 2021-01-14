30b27fb6561a097191317576203139ca718ec85c
prathameshThakur <prathamesh272000@gmail.com>
2021-01-14 20:29:22 +0530

    Add extra information

--- SendGrid/send-mail.py
+++ SendGrid/send-mail.py
@@ -1,3 +1,4 @@
+# After signing with Twilio SendGrid head-on to the code here!
 # using SendGrid's Python Library
 # https://github.com/sendgrid/sendgrid-python
 
@@ -7,6 +8,7 @@ from sendgrid.helpers.mail import Mail
 
 message = Mail(
     from_email='from_email@example.com',
+    # enter the email address to whome you want to send a mail  
     to_emails='to@example.com',
     subject='Sending with Twilio SendGrid is Fun',
     html_content='<strong>Those challenges are amazing!!</strong>')
